7123add3f770ec4edcfb0f20f460bec85908664e
Lucas Bastino <lucasbastino.dev@gmail.com>
2024-03-21 13:56:27 -0300

    iterate map ej 1 added

--- ejercicios/alumnos/main.go
+++ ejercicios/alumnos/main.go
@@ -2,32 +2,49 @@ package main
 
 import "fmt"
 
-type Parcial struct {
-	materia string
-	nota    int
-}
-
-type Materia struct {
-	nombre    string
-	parciales []Parcial
-	// final     int
-}
-
 type Alumno struct {
-	nombre   string
-	edad     int
-	materias []Materia
+	nombre string
+	edad   int
+	notas  map[string][]int
 }
 
 func main() {
 
-	crearDatos()
+	alumno1 := Alumno{"Lucas", 27, map[string][]int{}}
+	alumno1.notas["Matematica"] = []int{5, 8, 7, 9}
+	alumno1.notas["Geografia"] = []int{4, 10, 5, 8}
+	fmt.Println(alumno1)
+	imprimirParciales(alumno1)
+	imprimirTodosLosParciales(alumno1)
+	sumarleNota(alumno1)
 }
 
-func crearDatos() {
-	parcial1 := Parcial{"Matematica", 2}
-	fmt.Println(parcial1)
-	materia1 := Materia{"Matematica", []Parcial{parcial1}}
-	fmt.Println(materia1)
-	// alumno1 := Alumno("Lucas", 27, [materia1, materia2])
+func imprimirParciales(alumno Alumno) {
+	fmt.Println(alumno.nombre, alumno.edad)
+	for key, value := range alumno.notas {
+		fmt.Println(key, value)
+	}
+	fmt.Println(alumno.notas)
+}
+
+func imprimirTodosLosParciales(alumno Alumno) {
+	fmt.Println(alumno.nombre, alumno.edad)
+	for key, value := range alumno.notas {
+		fmt.Println(key)
+		for i := 0; i < len(value); i++ {
+			fmt.Println(value[i])
+		}
+	}
+}
+
+func sumarleNota(alumno Alumno) {
+	fmt.Println("sumarle nota")
+	for _, value := range alumno.notas {
+		for i, nota := range value {
+			nota++
+			fmt.Println(nota)
+			fmt.Println(value[i])
+		}
+	}
+	fmt.Println(alumno.notas)
 }
